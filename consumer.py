1766cc63c94c97d6a53609310438d78f898c936e
Adnan Majeed <49750395+adnanmajeed82@users.noreply.github.com>
2026-06-03 16:28:31 +0500

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,1 +1,16 @@
+from kafka import KafkaConsumer
+import json
 
+consumer = KafkaConsumer(
+    "student-topic",
+    bootstrap_servers="localhost:9092",
+    auto_offset_reset="earliest",
+    group_id="student-group",
+    value_deserializer=lambda m: json.loads(m.decode("utf-8"))
+)
+
+print("Waiting for messages...")
+
+for message in consumer:
+    print("\nStudent Received:")
+    print(message.value)
